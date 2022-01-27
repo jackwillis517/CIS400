4dc7c6b05ef9e8f040d6b065e4ac2ebbfd957323
jackwillis517 <jackwillis517@gmail.com>
2022-01-26 20:41:46 -0500

    New start

---
+++ CIS400/Tests/lecture1Tests/Lecture1Test.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Lecture1Test {
+    public static void main(String[] args) {
+        test1();
+    }
+
+    @Test
+    public static void test1() {
+        Assertions.assertEquals("Hell", Lecture1.speak());
+    }
+}
---
+++ CIS400/src/Lecture1.java
@@ -0,0 +1,9 @@
+public class Lecture1 {
+    public static void main(String[] args) {
+        System.out.println(speak());
+    }
+
+    public static String speak() {
+        return "Hello";
+    }
+}
